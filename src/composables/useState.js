a3631f9615e072cd54541bca520e71315462da25
CCBaxter84 <troyceclark@gmail.com>
2024-06-04 10:30:22 -0600

    Added refObject

--- src/composables/useState.js
+++ src/composables/useState.js
@@ -1,10 +1,15 @@
-import { reactive } from 'vue'
+import { reactive, ref } from 'vue'
 
 export function useState() {
   const reactiveObject = reactive({
     id: 1,
     name: 'reactiveObject'
   })
+  
+  const refObject = ref({
+    id: 2,
+    name: 'refObject'
+  })
 
-  return { reactiveObject }
+  return { reactiveObject, refObject }
 }
