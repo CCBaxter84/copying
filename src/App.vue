260b81f4f6df6c032f33186ce0d250980c859516
CCBaxter84 <troyceclark@gmail.com>
2024-06-04 11:02:56 -0600

    window.postMessage testing added

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,20 @@
 <script setup>
+import { onMounted, toRaw } from 'vue'
+import { useState } from './composables/useState'
 import JsonParse from './components/JsonParse.vue'
 import StructuredClone from './components/StructuredClone.vue'
 import Lodash from './components/Lodash.vue'
+
+const { reactiveObject, refObject } = useState()
+
+onMounted(() => {
+  window.addEventListener("message", e => {
+    e.data?.item && console.log(e.data.item)
+  })
+  window.postMessage({ item: { message: "POJO test" }})
+  window.postMessage({ item: toRaw(refObject.value) })
+  window.postMessage({ item: toRaw(reactiveObject) })
+})
 </script>
 
 <template>
