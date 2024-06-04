2609ddad42c274b64eff157f98c660a1bca3312e
CCBaxter84 <troyceclark@gmail.com>
2024-06-04 09:50:05 -0600

    Added naive JsonParse.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
+import JsonParse from './components/JsonParse.vue'
 </script>
 
 <template>
-  <h1>Copying in Vue 3</h1>
+  <h1 style="color: green;">Copying in Vue 3</h1>
+  <json-parse></json-parse>
 </template>
 
---
+++ src/components/JsonParse.vue
@@ -0,0 +1,19 @@
+<template>
+  <section style="color: blue;">
+    <h2>JSON.parse & JSON.stringify</h2>
+    <article>original: {{ reactiveObject }}</article>
+    <article>copy: {{ copiedReactiveObject }}</article>
+  </section>
+</template>
+
+<script setup>
+import { reactive, computed } from 'vue'
+
+const reactiveObject = reactive({
+  id: 1,
+  name: 'reactiveObject'
+})
+
+const copiedReactiveObject = computed(
+  () => JSON.parse(JSON.stringify(reactiveObject)))
+</script>
