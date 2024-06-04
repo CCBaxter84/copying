5e4b6da752dcb834b29b5604371387a73e2e9ef4
CCBaxter84 <troyceclark@gmail.com>
2024-06-04 09:57:58 -0600

    Created template Card component

---
+++ src/components/Card.vue
@@ -0,0 +1,12 @@
+<template>
+  <section>
+    <h2>JSON.parse & JSON.stringify</h2>
+    <article>Original: {{ originalObject }}</article>
+    <article>Copy: {{ copiedObject }}</article>
+    <article>Original === Copy: {{ originalObject === copiedObject }}</article>
+  </section>
+</template>
+
+<script setup>
+defineProps(["originalObject", "reactiveObject", "copiedObject"])
+</script>
--- src/components/JsonParse.vue
+++ src/components/JsonParse.vue
@@ -1,14 +1,12 @@
 <template>
-  <section style="color: blue;">
-    <h2>JSON.parse & JSON.stringify</h2>
-    <article>Original: {{ reactiveObject }}</article>
-    <article>Copy: {{ copiedReactiveObject }}</article>
-    <article>Original === Copy: {{ reactiveObject === copiedReactiveObject }}</article>
-  </section>
+<card :original-object="reactiveObject"
+      :copied-object="copiedReactiveObject"
+      style="color: blue;"/>
 </template>
 
 <script setup>
 import { reactive, computed } from 'vue'
+import Card from './Card.vue'
 
 const reactiveObject = reactive({
   id: 1,
