d6a2a41e16e0def9c0e1cb4ddef2f2d79a1b7a01
CCBaxter84 <troyceclark@gmail.com>
2024-06-04 10:33:30 -0600

    Converted structured clone

--- src/components/StructuredClone.vue
+++ src/components/StructuredClone.vue
@@ -1,8 +1,13 @@
 <template>
-  <card :original-object="reactiveObject"
-        :copied-object="clonedReactiveObject"
-        title="Structured Clone"
-        style="color: red;"/>
+  <section style="margin-bottom: 3rem; color: red;">
+    <h2>Structured Clone</h2>
+    <card :original-object="reactiveObject"
+          :copied-object="clonedReactiveObject"
+          title="JSON.parse & JSON.stringify"/>
+    <card :original-object="refObject"
+          :copied-object="copiedRefObject"
+          title="JSON.parse & JSON.stringify"/>
+  </section>
 </template>
 
 <script setup>
@@ -10,11 +15,18 @@ import { computed } from 'vue'
 import { useState } from '../composables/useState'
 import Card from './Card.vue'
 
-const { reactiveObject } = useState()
+const { reactiveObject, refObject } = useState()
 
 const clonedReactiveObject = computed(() => {
   try {
     return structuredClone(reactiveObject)
+  } catch(e) {
+    return e
+  }
+})
+const copiedRefObject = computed(() => {
+  try {
+    return structuredClone(refObject.value)
   } catch(e) {
     return e
   }
