329160251f78aad27fc143efd13a3c26730eebc0
CCBaxter84 <troyceclark@gmail.com>
2024-06-04 10:36:49 -0600

    Updated Lodash w/ref

--- src/components/Lodash.vue
+++ src/components/Lodash.vue
@@ -1,8 +1,11 @@
 <template>
-  <card :original-object="reactiveObject"
-        :copied-object="clonedReactiveObject"
-        title="Lodash cloneDeep"
-        style="color: purple;"/>
+  <section style="margin-bottom: 3rem; color: purple;">
+    <h2>Lodash cloneDeep</h2>
+    <card :original-object="reactiveObject"
+          :copied-object="clonedReactiveObject"/>
+    <card :original-object="refObject"
+          :copied-object="clonedRefObject"/>
+  </section>
 </template>
 
 <script setup>
@@ -11,11 +14,19 @@ import { useState } from '../composables/useState'
 import { cloneDeep } from 'lodash'
 import Card from './Card.vue'
 
-const { reactiveObject } = useState()
+const { reactiveObject, refObject } = useState()
 
 const clonedReactiveObject = computed(() => {
   try {
     return cloneDeep(reactiveObject)
+  } catch(e) {
+    return e
+  }
+})
+
+const clonedRefObject = computed(() => {
+  try {
+    return cloneDeep(refObject.value)
   } catch(e) {
     return e
   }
