3face1a5ff5ad27d0c5e8aa06651adf388e3265b
CCBaxter84 <troyceclark@gmail.com>
2024-06-06 11:05:29 -0600

    Added Card for Circular DTO

---
+++ src/components/CircularReferenceCard.vue
@@ -0,0 +1,12 @@
+<template>
+  <section>
+    <h3>Circular Reference</h3>
+    <article>{{ circular?.value }}</article>
+    <article>{{ circularCopy?.value }}</article>
+    <article>Original === Copy: {{ circular === circularCopy }}</article>
+  </section>
+</template>
+
+<script setup>
+defineProps(["circular", "circularCopy"])
+</script>
